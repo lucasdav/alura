11832a99cb821a16dd9f6d28ddbebdbed4b5a302
unknown <lucassouzadavanso@gmail.com>
2019-06-20 12:46:50 -0300

    alura angular + typescript

---
+++ aula-typescript/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,101 @@
+import { NegociacoesView, MensagemView } from '../views/index';
+import { Negociacoes, Negociacao } from '../models/index';
+import { domInject, throttle } from '../helpers/decorators/index';
+import { NegociacaoParcial } from '../models/index';
+import { NegociacaoService } from '../services/NegociacaoService';
+import { imprime } from '../helpers/index';
+
+export class NegociacaoController {
+    
+    @domInject('#data')
+    private _inputData: JQuery;
+
+    @domInject('#quantidade')
+    private _inputQuantidade: JQuery;
+
+    @domInject('#valor')
+    private _inputValor: JQuery;
+    private _negociacoes: Negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
+    private _mensagemView = new MensagemView('#mensagemView');
+
+    private _service = new NegociacaoService();
+
+    constructor() {
+        this._negociacoesView.update(this._negociacoes);
+    }
+
+    @throttle(500)
+    adiciona() {
+                
+        let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+        if(!this._ehDiaUtil(data)) {
+
+            this._mensagemView.update("Negociações apenas para dias úteis, por favor!");
+            return
+        }
+
+        const negociacao = new Negociacao(
+            data,
+            parseInt(this._inputQuantidade.val()),
+            parseFloat(this._inputValor.val())
+            
+        );
+
+        this._negociacoes.adiciona(negociacao);
+        imprime(negociacao, this._negociacoes);
+        this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update('Negociação adicionada com sucesso!');
+    }
+
+    private _ehDiaUtil(data: Date) {
+
+        return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+    }
+
+    @throttle(500)
+    async importarDados() {
+
+        try{
+
+        const negociacoesParaImportar = await this._service
+        .obterNegociacoes(res => {
+
+            if(res.ok) {
+                return res;
+            } else {
+                throw new Error(res.statusText);
+            }
+        });
+
+        const negociacoesJaImportada = this._negociacoes.paraArray();
+
+        negociacoesParaImportar
+            .filter(negociacao => 
+                !negociacoesJaImportada.some(jaImportada => 
+                    negociacao.ehIgual(jaImportada)))
+            .forEach(negociacao => 
+            this._negociacoes.adiciona(negociacao));
+
+        this._negociacoesView.update(this._negociacoes);
+
+        } catch(err) {
+            this._mensagemView.update(err.message)
+        }
+
+        
+       
+    }
+}
+
+enum DiaDaSemana {
+
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
+}
