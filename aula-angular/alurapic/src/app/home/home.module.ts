3d7fc462c960f6b1ab53c2f040d811cf46088cfa
unknown <lucassouzadavanso@gmail.com>
2019-08-14 21:05:33 -0300

    alurapic folder

---
+++ aula-angular/alurapic/src/app/home/home.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SignInComponent } from './signin/signin.component';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { SignUpComponent } from './signup/signup.component';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+    declarations: [ 
+        SignInComponent,
+        SignUpComponent,
+        HomeComponent
+     ],
+    imports: [ 
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        RouterModule
+    ]
+})
+
+export class HomeModule {}
---
+++ aula-angular/alurapic/src/app/home/signup/signup.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+import { NewUser } from './new-user';
+import { SignUpService } from './signup.service';
+import { Router } from '@angular/router';
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
+
+@Component({
+    templateUrl: './signup.component.html'
+})
+export class SignUpComponent implements OnInit {
+
+    signupForm: FormGroup;
+    @ViewChild('inputEmail') inputEmail: ElementRef<HTMLInputElement>;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private userNotTakenValidatorService: UserNotTakenValidatorService, 
+        private signUpService: SignUpService,
+        private router: Router,
+        private platformDetectorService: PlatformDetectorService) {}
+
+        ngOnInit(): void {
+            this.signupForm = this.formBuilder.group({
+                email: ['',
+                    [
+                        Validators.required,
+                        Validators.email
+                    ]
+                ],
+                userName: ['', 
+                    [
+                        Validators.required,
+                        lowerCaseValidator,
+                        Validators.minLength(2),
+                        Validators.maxLength(30)
+                    ],
+                    this.userNotTakenValidatorService.checkUserNameTaken()
+                ],
+                fullName: ['', 
+                    [
+                        Validators.required,
+                        Validators.minLength(2),
+                        Validators.maxLength(40)
+                    ]
+                ],
+                password: ['', 
+                    [
+                        Validators.required,
+                        Validators.minLength(8),
+                        Validators.maxLength(14)
+                    ]
+                ]
+            });
+
+            this.platformDetectorService.isPLatformBrowser() &&
+            this.inputEmail.nativeElement.focus();
+        }
+
+    signup() {
+        const newUser = this.signupForm.getRawValue() as NewUser;
+        this.signUpService
+            .signup(newUser)
+            .subscribe(
+                () => this.router.navigate(['']),
+                err => console.log(err)
+            );
+    }
+    
+}
